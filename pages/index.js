2cae5a2321373c4ecc0e455f9eab6baa78a6caeb
Karan Jaiswal <jaiskaran008@gmail.com>
2024-07-26 10:23:14 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+//default html page, endpoint- our-domain.com/
+function HomePage(){
+    return (
+        <h1>The home page</h1>
+    )
+}
+export default HomePage;
---
+++ pages/news.js
@@ -0,0 +1,8 @@
+//our-domain.com/news
+//default html page, endpoint- our-domain.com/
+function NewsPage(){
+    return (
+        <h1>The News page</h1>
+    )
+}
+export default NewsPage;
